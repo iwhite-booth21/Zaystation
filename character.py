3f27adda5abaf2c1c9742ea7d26efdbb919f7db1
iwhite-booth21 <booth2116@outlook.com>
2023-06-25 22:59:37 -0400

    Commit before device swap

--- character.py
+++ character.py
@@ -1,8 +1,3 @@
-#Lets Try a Number System for Character Conditions
-from character import Character, player_status
-
-
-
 
 class Status:
     def __init__(self, condition):
@@ -59,26 +54,63 @@ class Character:
         self.ability = ability
         self.armor = armor 
         self.status = status
+    
+    @property
+    def damage(self):
+        return self.health
+    
+    @damage.setter
+    def damage(self, new_health):
+        if new_health != self.health:
+            self.health = new_health
+    def damage_taken(self, damage):
+        self.health -= damage
+        return self.health
+    def get_health(self):
+        return int(self.health)
         
 
 
 # main code area
 
 Default_Characters = [
-    Character("Eric the Barbarian", "Eldian", 25, 100, "Super Perception", "Ebony Armor", player_status),
-    Character("Gladys the Healer", "Noble", 15, 95, "Party Heal", "Angel Fabric", player_status),
-    Character("Edward Elric", "Alchemist Elite",75,100, "Transmute", player_status),
-    Character("Jade the Battler", "Orc", 10, 85, "Bereserk", "Battle Grieves", player_status) 
+    Character("Eric the Barbarian", "Eldian", 25, 100, "Super Perception", "Ebony Armor", Status(2)),
+    Character("Gladys the Healer", "Noble", 15, 95, "Party Heal", "Angel Fabric", Status(2)),
+    Character("Edward Elric", "Alchemist Elite",75,100, "Transmute", "Tunic", Status(2)),
+    Character("Jade the Battler", "Orc", 10, 85, "Bereserk", "Battle Grieves", Status(2)) 
     ]
 
 Default_Enemies = [
-    
+    Enemy("Goblin", "Wooden Armor", Status(1), 8),
+    Enemy("Hobgoblin", "Stone Armor", Status(1), 10),
+    Enemy("Homonculus", "Skin of dead", Status(1), 20),
+    Enemy("Bowser", "Spiky Shell", Status(1), 18)
 ]
+
+## Characters
 
 Eric = Default_Characters[0]
 Gladys = Default_Characters[1]
 Edward = Default_Characters[2]
 Jade = Default_Characters[3]
+
+## Enemies
+Goblin = Default_Enemies[0]
+Hobgoblin = Default_Enemies[1]
+Homonculus = Default_Enemies[2]
+Bowser = Default_Enemies[3]
+
+print("Some Default Hero Statuses")
+## View Character and Enemy Status
+print(Eric.status.get_cond_value())
+print(Gladys.status.get_cond_value())
+
+print(Goblin.status.get_cond_value())
+print(Bowser.status.get_cond_value())
+
+##Test Increment and Decrement Health
+# print(Eric.damage_taken(15))
+# print(Homonculus.damage_taken(20))
 
 
 Enemy_Status = Status(3)
@@ -87,9 +119,4 @@ print(Gogoblin.damage_taken(20))
 
 player_status = Status(5)      
 BillyBob = Character("Jerry", "Brenton", 5, 100, "Fire Fist", "None", player_status)
-print(BillyBob.status.get_cond_value())
-
-
-player_status.decrease_Cond(1)
-BillyBob = Character("Brenton", 5, 100, "Fire Fist", "None", player_status)
 print(BillyBob.status.get_cond_value())
